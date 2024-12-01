5257eecc863d3dfb6d98324335286acca1206533
Bondzik-S <bondziks@gmail.com>
2024-12-01 17:07:37 +0200

    add restaurant app

---
+++ restaurant/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+from accounts.models import Cook
+
+
+class DishType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, related_name="dishes")
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+
+    def __str__(self):
+        return self.name
