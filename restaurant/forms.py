0c423b51d7ac95c3a55e3b67fe2887e7c3f75d96
Bondzik-S <bondziks@gmail.com>
2024-12-04 13:34:15 +0200

    added forms

---
+++ restaurant/forms.py
@@ -0,0 +1,84 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.core.exceptions import ValidationError
+
+from accounts.models import Cook
+from restaurant.models import Dish
+
+
+class CookCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Cook
+        fields = (UserCreationForm.Meta.fields
+                  + ("years_of_experience", "first_name", "last_name"))
+
+    def clean_years_of_experience(self):
+        return validate_years_of_experience(self.cleaned_data["years_of_experience"])
+
+
+class CookUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Cook
+        fields = (UserCreationForm.Meta.fields
+                  + ("years_of_experience", "first_name", "last_name"))
+
+    def clean_years_of_experience(self):
+        return validate_years_of_experience(self.cleaned_data["years_of_experience"])
+
+
+class DishForm(forms.ModelForm):
+    cooks = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
+    class Meta:
+        model = Dish
+        fields = ["name", "description", "price", "dish_type", "cooks"]
+
+
+class CookSearchForm(forms.Form):
+    username = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(attrs={"placeholder": "Search by username"})
+    )
+
+
+class DishSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by name"
+            }
+        )
+    )
+
+
+class DishTypeSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "Search by name"
+            }
+        )
+    )
+
+
+def validate_years_of_experience(
+    years_of_experience,
+):
+    if years_of_experience <= 0:
+        raise ValidationError("Years of experience cannot be less than 1")
+    elif years_of_experience > 55:
+        raise ValidationError("Years of experience cannot be more than 55")
+
+    return years_of_experience
