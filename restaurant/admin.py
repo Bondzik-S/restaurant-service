83e7cbed59ee421b741d9870e837f82d8d0c3433
Bondzik-S <bondziks@gmail.com>
2024-12-04 13:33:29 +0200

    configured admin panel

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Dish, DishType
+from accounts.models import Cook
 
-# Register your models here.
+
+@admin.register(Cook)
+class CookAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("years_of_experience",)}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Additional info",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "years_of_experience",
+                )
+            },
+        ),
+    )
+
+
+@admin.register(DishType)
+class DishTypeAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "dish_type")
+    list_filter = ("dish_type",)
+    search_fields = ("name", "dish_type__name")
+    filter_horizontal = ("cooks",)
